b47580a1405ca35dfad34fa73c342b4dd59e363b
marioCarvalho001 <84389288+marioCarvalho001@users.noreply.github.com>
2022-05-16 10:45:29 -0300

    ok

--- api.js
+++
@@ -1,8 +0,0 @@
-function reqListener () {
-  console.log(this.responseText);
-};
-
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "yourFile.txt", true);
-oReq.send();
